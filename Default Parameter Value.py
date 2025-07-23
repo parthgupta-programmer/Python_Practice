a6de9492503a74c557d5b4b613140aab896eb71a
parthgupta-programmer <parthg.info@gmail.com>
2025-07-23 12:55:58 +0530

    Using Default Value in a function

---
+++ Default Parameter Value.py
@@ -0,0 +1,5 @@
+def name(nm="/enter your Name here/"):
+    print("My name is " + nm)
+name("Parth Gupta")
+name("Mahendra Singh Dhoni")
+name() # If we call the function without any argument then it uses the default value
